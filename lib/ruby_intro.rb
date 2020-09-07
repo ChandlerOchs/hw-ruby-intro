bf0f4efb40b58f234ec397dd24dcb55acc5e2c30
EC2 Default User <ec2-user@ip-172-31-72-156.ec2.internal>
2020-09-07 20:39:20 +0000

    Completed part 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,14 +4,49 @@
 
 def sum arr
   # YOUR CODE HERE
+  sum = 0
+  if !arr.empty?
+    arr.each { |val|
+      sum += val
+    }
+  end
+  return sum
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  
+  sum = 0
+  if !arr.empty?
+    if arr.length() == 1
+      return arr[0]
+    end
+    max1 = arr[0]
+    max2 = arr[1]
+    arr = arr.slice(2, arr.length())
+    arr.each { |val|
+      if val > max1
+        max1 = val
+      elsif val > max2
+        max2 = val
+      end
+    }
+    sum = max1 + max2
+  end
+  return sum
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  options = Set.new
+  arr.each { |val|
+    if options.include?(n-val)
+      return true
+    else
+      options << val
+    end
+  }
+  return false
 end
 
 # Part 2
